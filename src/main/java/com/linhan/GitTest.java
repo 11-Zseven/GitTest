a095208a03b7b535ebc98039b2dfd8f930019971
root <root@qq.com>
2022-07-05 22:34:46 +0800

    git-test

---
+++ src/main/java/com/linhan/GitTest.java
@@ -0,0 +1,12 @@
+package com.linhan;
+/**
+ *@program: GitTest
+ *@description:
+ *@author: lin han
+ *@date: 2022/07/05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git.Test");
+    }
+}
