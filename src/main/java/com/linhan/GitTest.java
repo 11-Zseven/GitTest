b489c82fb16af282cdb57f4f23ccbe3a6fec228e
root <root@qq.com>
2022-07-06 21:13:43 +0800

    push commit

--- src/main/java/com/linhan/GitTest.java
+++ src/main/java/com/linhan/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("linhan-branch 1");
         System.out.println("master conflict test");
         System.out.println("linhan-branch conflict test");
+        System.out.println("push test");
     }
 }
