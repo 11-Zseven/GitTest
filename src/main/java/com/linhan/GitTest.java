5f324fa2619b1617c9b22e88d5c3d3586d9265ac
11-Zseven <96112575+11-Zseven@users.noreply.github.com>
2022-07-06 21:17:14 +0800

    Update GitTest.java
    add pull test

--- src/main/java/com/linhan/GitTest.java
+++ src/main/java/com/linhan/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master conflict test");
         System.out.println("linhan-branch conflict test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
